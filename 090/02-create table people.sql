f5cb614676cfc09e76e5a299cce40db0c2a801f2
Osvaun <osvaun@gmail.com>
2023-02-28 11:35:02 -0800

    .vscode/settings.json

---
+++ 090/02-create table people.sql
@@ -0,0 +1,4 @@
+create table people (
+    pid SERIAL PRIMARY KEY,
+    pFirst VARCHAR(50) NOT NULL
+);
---
+++ 090/03-create table lists.sql
@@ -0,0 +1,5 @@
+CREATE TABLE lists(
+    lid SERIAL PRIMARY KEY,
+    pid INT REFERENCES people NOT NULL,
+    lname VARCHAR(50) NOT NULL
+);
